8ea19be5f8e2c48e72a28b26b387b3088f1a8084
ed.zhang <ed.zhang@W4029231.ca0375.muenchen.musin.de>
2017-05-08 10:24:16 +0200

    Added Tile Manager

---
+++ Was Bin ich/src/was/bin/ich/SpielVerwalter.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package was.bin.ich;
+
+/**
+ *
+ * @author ed.zhang
+ */
+import javafx.scene.image.*;
+import java.util.Random;
+
+public class SpielVerwalter {
+    Image tile = new Image("link");
+    private boolean Tiles [][] = new boolean [6][5];
+    private Random r = new Random();
+    
+    public SpielVerwalter(){
+        for(int i = 0; i<Tiles.length; i++){
+            for(int j = 0; j<Tiles[0].length; j++){
+                Tiles[i][j] = true;
+            }
+        }
+    }
+    
+    public void SelectRandomTile(){
+        boolean con = true;
+        while(con == true){
+            if(Tiles[r.nextInt(Tiles.length)][r.nextInt(Tiles[0].length)] == true){
+               Tiles[r.nextInt(Tiles.length)][r.nextInt(Tiles[0].length)] = false;
+               con = false;
+            }
+        }
+    }
+    
+    public boolean[][] Abfrage(){
+        return Tiles;
+    }
+    
+    public void Reset(){
+        for(int i = 0; i<Tiles.length; i++){
+            for(int j=0; j<Tiles[0].length; j++){
+                Tiles[i][j] = true;
+            }
+        }
+    }
+    
+}
